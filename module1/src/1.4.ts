5530feafb0a174f1f820959ece937cfe97613b89
arifkhan101 <arifkhan230122@gmail.com>
2024-05-01 21:23:25 +0600

    Learned about ts data types

---
+++ module1/src/1.4.ts
@@ -0,0 +1,28 @@
+// Basic Data Type
+
+// String
+let firstName: string = "Arif";
+
+// number
+let roll: number = 123;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined 
+let x: undefined = undefined;
+
+// null 
+let y: null = null;
+
+let d: number;
+
+d = 113;
+
+let friends: string[] = ["Shakil", "Juwel"];
+let eligibleRollList: number[] = [1,3,];
+
+// tuple --> array --> order --> type of values
+
+let coordinates: [number,number] = [1,5];
+let ageName:[number,string] = [50, "Mr.X"];
