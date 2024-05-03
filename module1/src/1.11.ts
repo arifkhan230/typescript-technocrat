7da1ac07cfec6d19e4b0217c390284dd635c26c0
arifkhan101 <arifkhan230122@gmail.com>
2024-05-03 23:10:11 +0600

    ternary operator optional chaining nullish coalescing operator

---
+++ module1/src/1.11.ts
@@ -0,0 +1,48 @@
+{
+    // ternary operator || optional chaining || nullish coalescing operator
+
+    const age:number = 15;
+
+    if(age>=18){
+        console.log("adult");
+    }else{
+        console.log("not adult");
+    }
+
+    const isAdult = age >= 18 ? "adult" : "not adult";
+    // console.log(isAdult);
+
+
+    // nullish coalescing operator
+    // null / undefined ----> decision making
+
+    const isAuthenticated = null;
+
+    const result1 = isAuthenticated ?? "Guest" ;
+    const result2 = isAuthenticated ? isAuthenticated : "Guest"
+    console.log({result1}, {result2});
+
+
+    type User ={
+        name:string,
+        address:{
+            city:string,
+            road:string,
+            presentAddress:string,
+            permanentAddress ?: string
+        }
+    }
+
+    const user: User = {
+        name: "Arif",
+        address:{
+            city:"Moulvibazar",
+            road:"awesome road",
+            presentAddress:"Matarkapon",
+        }
+    }
+
+    const permanentAddress = user?.address?.permanentAddress?? "no permanent address" ;
+    console.log({permanentAddress});
+
+}
