8fc50a8885c33d04452d2ce249c3c1c74d6a6cab
arifkhan101 <arifkhan230122@gmail.com>
2024-05-04 12:04:02 +0600

    interface, type vs interface

---
+++ module2/2.2.ts
@@ -0,0 +1,48 @@
+{
+    // 
+    type User1 = {
+        name: string;
+        age: number;
+    }
+
+    interface User2 {
+        name: string;
+        age: number;
+    }
+
+    type UserWithRoll1 = User1 & { role: string }
+
+    interface UserWithRoll2 extends User2 {
+        role: string
+    }
+
+    const user1: UserWithRoll2 = {
+        name: "Khan",
+        age: 100,
+        role: "manager"
+    }
+
+
+    type rollNumber = number;
+
+    // js---> object , array -> object function -> object
+
+    type Roll1 = number[];
+
+    interface Roll2 {
+        [index: number]: number
+    }
+
+    const rollNumber1: Roll2 = [1, 2, 3];
+
+    type Add1 = (num1: number, num2: number) => number;
+
+    interface Add2{
+        (num1:number,num2:number):number
+    }
+
+    const add: Add2 = (num1, num2) => num1 + num2;
+
+
+    // 
+}
