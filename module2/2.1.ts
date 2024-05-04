00564b887fa6f282dbfa5bb29456d89108acefd4
arifkhan101 <arifkhan230122@gmail.com>
2024-05-04 06:55:23 +0600

    explored type assertion

---
+++ module2/2.1.ts
@@ -0,0 +1,38 @@
+{
+    // 
+
+
+    // type assertion
+    let anything: any;
+    anything = "Next Level Web Development";
+
+    anything = 123;
+
+    (anything as number);
+
+    const kgTogGm = (value: string | number):  string | number | undefined => {
+        if (typeof value === "string") {
+            const convertedValue = parseFloat(value) * 1000;
+            return `The converted value is : ${convertedValue}`;
+        }
+        if (typeof value === "number") {
+            return value * 1000;
+        }
+    }
+
+    const result1 = kgTogGm(1000) as number;
+    const result2 = kgTogGm("1000") as string;
+
+    type CustomError ={
+        message: string
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log((error as CustomError).message);
+    }
+
+
+    // 
+}
