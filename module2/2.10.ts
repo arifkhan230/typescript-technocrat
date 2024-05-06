3e2004ccc913402f1201286f02bd9e77b00bf63e
arifkhan101 <arifkhan230122@gmail.com>
2024-05-06 15:02:24 +0600

    mapped type

---
+++ module2/2.10.ts
@@ -0,0 +1,33 @@
+{
+    // mapped types
+
+    const arrOfNumber: number[] = [1, 4, 5];
+
+    // const arrOfString:string[] = ['1','4','5'];
+    const arrOfString: string[] = arrOfNumber?.map(number => number.toString());
+
+    console.log(arrOfString);
+
+    type AreaNumber = {
+        height:number;
+        width:number;
+    };
+
+    type Height = AreaNumber["height"]  //look up type
+    // type AreaString = {
+    //     height:string;
+    //     width:string
+    // }
+
+    type AreaString<T> ={
+        [key in keyof T] : T[key]
+    }
+
+    const area1 :AreaString<{height:string; width:number}> ={
+        height: "100",
+        width:50
+    }
+
+
+    // 
+}
